76f33432b6929ff465fb728d0692ff9774646d45
300000rb <300000rb@gmail.com>
2025-05-22 05:21:50 +0330

    Initial commit

--- index.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "سلام! اپلیکیشن روی Vercel اجرا شد."
-
-# نیازی به if __name__ == "__main__": نیست چون Vercel از آن استفاده نمی‌کند
